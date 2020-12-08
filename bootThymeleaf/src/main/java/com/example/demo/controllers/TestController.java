9ddda16453c191377b49771c8465fa2afa95d011
shanth5100 <prashanth.veeraboena@gmail.com>
2020-12-08 18:34:55 +0530

    0.1: html loaded by thymeleaf

--- bootThymeleaf/src/main/java/com/example/demo/controllers/TestController.java
+++ bootThymeleaf/src/main/java/com/example/demo/controllers/TestController.java
@@ -2,12 +2,22 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
+//	@RequestMapping("hello")
+//	public void main() {
+//		System.out.println("Hello controller");
+//	}
+	
 	@RequestMapping("hello")
-	public void main() {
+//	@ResponseBody // making the application to return data
+	public String main() {
 		System.out.println("Hello controller");
+//		return "NewFile.jsp";
+		return "NewFile";
 	}
+
 }
