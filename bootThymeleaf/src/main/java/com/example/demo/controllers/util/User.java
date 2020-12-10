de7b277362cbfbc2b25c06cc901860242a9f2f72
shanth5100 <prashanth.veeraboena@gmail.com>
2020-12-10 18:58:18 +0530

    0.1: error resolved

--- bootThymeleaf/src/main/java/com/example/demo/controllers/util/User.java
+++ bootThymeleaf/src/main/java/com/example/demo/controllers/util/User.java
@@ -1,15 +1,30 @@
 package com.example.demo.controllers.util;
 
-import java.util.Date;
-
 public class User
-//extends EmailGenarate
 {
 	private long id;
 	private String username;
+	private String password;
+	private String email;
 	private String name;
 	private boolean marrried;
-	private Date dob;
+//	private Date dob;
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	public long getId() {
 		return id;
@@ -43,18 +58,17 @@ public class User
 		this.marrried = marrried;
 	}
 
-	public Date getDob() {
-		return dob;
-	}
-
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
+//	public Date getDob() {
+//		return dob;
+//	}
+//
+//	public void setDob(Date dob) {
+//		this.dob = dob;
+//	}
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", name=" + name + ", marrried=" + marrried + ", dob="
-				+ dob + "]";
+		return "User [id=" + id + ", username=" + username + ", name=" + name + ", marrried=" + marrried + "]";
 	}
 
 	
