70384b5a3e3eee83c5e785730aa5e4092d297522
shanth5100 <prashanth.veeraboena@gmail.com>
2020-12-14 20:32:45 +0530

    0.1: Error handling in spring boot thymeleaf

---
+++ bootThymeleaf/src/main/java/com/example/demo/ErrorModel.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+public class ErrorModel {
+
+	private Long status;
+	private String message;
+	private String url;
+	private String buttonText;
+	
+	public Long getStatus() {
+		return status;
+	}
+	public void setStatus(Long status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getButtonText() {
+		return buttonText;
+	}
+	public void setButtonText(String buttonText) {
+		this.buttonText = buttonText;
+	}
+	
+	
+}
--- bootThymeleaf/src/main/java/com/example/demo/controllers/ErrorController.java
+++ bootThymeleaf/src/main/java/com/example/demo/controllers/ErrorController.java
@@ -1,15 +1,62 @@
 package com.example.demo.controllers;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/error")
-public class ErrorController {
+//@RequestMapping("/error")
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController{
 
-	@GetMapping
-	public String errorPage() {
-		return "error";
+	private static final Logger LOGGER = LoggerFactory.getLogger(ErrorController.class);
+	
+//	@GetMapping
+//	public String errorPage() {
+//		return "error";
+//	}
+	
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
+
+	@RequestMapping("/error")
+	public String errorPage(HttpServletRequest req, Model model) {
+		
+		String pageTitle = "Error";
+		String errorPage = "error";
+		
+		Object status = req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		System.out.println(Integer.valueOf(status.toString()));
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+			
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				pageTitle =  "Page not found";
+				errorPage = "error/404";
+				LOGGER.error("Error 404");
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				pageTitle =  "Forbidden Error";
+				errorPage = "error/403";
+				LOGGER.error("Error 403");
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+					pageTitle =  "INTERNAL SERVER ERROR";
+					errorPage = "error/500";
+					LOGGER.error("Error 500");
+				}
+			
+		} 
+		
+		model.addAttribute("pageTitle", pageTitle);
+		
+		return errorPage;
+	}
+	
 }
