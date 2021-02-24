a560e2c74b65deaace8f8a2a9979733c0f202d5f
dev1 <dev1@124apps.com>
2021-02-24 21:45:42 +0530

    scriptfile for Roo MM pjt, but basic understand added here

---
+++ bootThymeleaf/testRun.sh
@@ -0,0 +1,36 @@
+dire="_runScript"
+pwDir () { 
+    curDir="${PWD##*/}"
+    echo "$curDir"
+}
+
+executeApp () {
+    cd ..
+    echo "${PWD##*/}"
+
+    mvn clean install > ./_runScript/mvn_install.log
+    installResu=$?
+    if [ "$installResu" = "0" ]
+    then
+        echo "install success"
+        appRunDir="application"
+        cd application
+        # pwDir
+        # if [ "$curDir" = "$appRunDir" ]
+        # then 
+            mvn -X springboot-boot:run > ../_runScript/mvn_run.log
+        # fi
+    else
+        echo "install fail"
+    fi
+} 
+
+# We need to declare a Func Def and execute.
+pwDir
+if [ "$curDir" = "$dire" ]
+then 
+    echo $curDir;
+    executeApp; 
+else
+    echo "not a run Dir"
+fi
